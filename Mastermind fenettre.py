1641826290417ce75fa27feede1d796ce0bc9426
hitlif <75883926+hitlif@users.noreply.github.com>
2025-03-11 21:12:07 +0100

    Update Mastermind fenetre.py

--- Mastermind fenettre.py
+++ Mastermind fenettre.py
@@ -1,13 +1,13 @@
-import tkinter as tk
+import tkinter as tk #biliotheque
 couleurs=['red','blue','green','yellow']
-def couleur_choisie(couleur):
+def couleur_choisie(couleur): #fonction pour choisir une couleur
     print(f"choisie la couleur:{couleur}")
-window=tk.Tk()
-window.title("mastermind")
-window.geometry("1280x720")#taille de la fenettre
-couleur_frame=tk.Frame(window)
+window=tk.Tk()#cree la fenetre
+window.title("mastermind")#titre de la fenetre (pas tres important)
+window.geometry("1280x720")#taille de la fenetre (meme chose que au dessus)
+couleur_frame=tk.Frame(window)#Frame en gros c'est une boite et dedans on peut peut mettre des truc
 couleur_frame.pack(pady=10)
-for couleur in couleurs:
+for couleur in couleurs:#cree les boutton
     bouton=tk.Button(couleur_frame,bg=couleur,width=5,height=2)#width c'est la largeur et height c'est la hauteur
     bouton.pack(side=tk.LEFT, padx=5)
 window.mainloop()#START GAME!
