9d7f8a4403e330eb0215605d55d36b97cd5b53a5
hitlif <75883926+hitlif@users.noreply.github.com>
2025-03-11 21:12:53 +0100

    Update Mastermind fenetre.py

--- Mastermind fenettre.py
+++ Mastermind fenettre.py
@@ -5,7 +5,7 @@ def couleur_choisie(couleur): #fonction pour choisir une couleur
 window=tk.Tk()#cree la fenetre
 window.title("mastermind")#titre de la fenetre (pas tres important)
 window.geometry("1280x720")#taille de la fenetre (meme chose que au dessus)
-couleur_frame=tk.Frame(window)#Frame en gros c'est une boite et dedans on peut peut mettre des truc
+couleur_frame=tk.Frame(window)#Frame en gros c'est une boite et dedans on peut peut mettre des texte ou des boutton
 couleur_frame.pack(pady=10)
 for couleur in couleurs:#cree les boutton
     bouton=tk.Button(couleur_frame,bg=couleur,width=5,height=2)#width c'est la largeur et height c'est la hauteur
