71c65ce802d318277b92f5fe01889f6518386c4d
fiorav <fionar.li78@gmail.com>
2025-03-16 16:59:23 +0100

    Choix du code

---
+++ Code.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+root=tk.Tk()
+root.title("MASTERMIND")
+
+frame=tk.Frame(root)
+frame.pack(fill=tk.BOTH, expand=True)
+
+def choix_code():
+    codes=[]
+    for i in range (4):
+        code=input("Choisir une couleur pour creer un code a quatre couleurs(faire cela 4 fois):")#permet a l'utilisateur qui choisi le code secret de le mmettre dedans 
+        codes.append(code) #met les couleurs choisis dans une liste 
+    return codes #a enlever plus tard mais la c'est pour tester qu'il renvoie bien le code secret de l'utilisateur
+print(choix_code())
+
+
+
